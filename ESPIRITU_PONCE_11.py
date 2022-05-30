28e89b47bec7d1112454b8f9cd4da329fea0252e
Galahallt <paolo_edni_v_espiritu@dlsu.edu.ph>
2022-05-30 12:27:09 +0800

    initialize proj

---
+++ ESPIRITU_PONCE_11.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+""" Understanding Process Synchronization
+
+In this project, the task is to create a solution to manage a number of
+people inside the fitting room of a department store.
+"""
+
+__authors__ = ["Paolo Espiritu", "Andre Ponce"]
+__email__ = ["paolo_edni_v_espiritu@dlsu.edu.ph", "andre_ponce@dlsu.edu.ph"]
+__date__ = "2022/05/30"
+__license__ = "MIT"
+__version__ = "1.0"
