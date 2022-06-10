cf0886f125be22f0486d2fc54a26a616c8d391b1
Galahallt <paolo_edni_v_espiritu@dlsu.edu.ph>
2022-06-10 19:33:10 +0800

    fixes on multithreading; no starvation yet

--- ESPIRITU_PONCE_11.py
+++ ESPIRITU_PONCE_11.py
@@ -16,61 +16,37 @@ import threading
 import time
 
 
-def change_thread():
-    global n, b, g
-    global ctr, blueCtr, greenCtr, quantum
-    global isBlue
-
-    # if there are still green and blue threads to be executed
-    if blueCtr < b and greenCtr < g:
-        # if quantum reached, switch to other color
-        if ctr == quantum:
-            ctr = 0
-            isBlue = not isBlue
-            print("------Empty Fitting Room------")
-        # else, just increment counter
-        else:
-            ctr += 1
-    # if there are no more green threads but there are still blue threads to be executed
-    elif greenCtr == g and not isBlue:
-        ctr = 0
-        isBlue = True
-        print("------Empty Fitting Room------")
-    # if there are no more blue threads but there are still green threads to be executed
-    elif blueCtr == b and isBlue:
-        ctr = 0
-        isBlue = False
-        print("------Empty Fitting Room------")
-    else:
-        ctr = 1
-
-
 def green_enter():
-    global fitting_room, green_access
+    global fitting_room, green_access, blue_access
     global greenCtr, blueCtr, b, g, quantum
 
     # wait for blue threads to release the lock for the fitting room
-    while blue_access.locked():
-        pass
+    while True:
+        if not blue_access.locked():
+            break
 
     # acquire the access to the fitting room for the green threads
     if not green_access.locked():
         green_access.acquire()
         print("----------Green Only----------")
 
-    with fitting_room:
-        print(threading.current_thread().name)
-        greenCtr += 1
-        time.sleep(1)
+    if green_access.locked():
+        with fitting_room:
+            print(threading.current_thread().name)
+            greenCtr += 1
+            time.sleep(1)
 
     # if all green threads are finished or the number of green threads
     # reached the quantum that was set, pass the access to blue threads
-    if greenCtr == g or greenCtr % quantum == 0:
+    if (greenCtr == g or greenCtr % quantum == 0) and green_access.locked():
         print("------Empty Fitting Room------")
         green_access.release()
 
-    while blue_access.locked() == False and blueCtr != b:
-        pass
+        while True:
+            if (blue_access.locked() and blueCtr) < b or (
+                blueCtr == b and greenCtr == 0
+            ):
+                break
 
 
 def blue_enter():
@@ -78,27 +54,32 @@ def blue_enter():
     global greenCtr, blueCtr, b, g, quantum
 
     # wait for green threads to release the lock for the fitting room
-    while green_access.locked():
-        pass
+    while True:
+        if not green_access.locked():
+            break
 
     # acquire the access to the fitting room for the blue threads
     if not blue_access.locked():
         blue_access.acquire()
         print("----------Blue Only-----------")
 
-    with fitting_room:
-        print(threading.current_thread().name)
-        blueCtr += 1
-        time.sleep(1)
+    if blue_access.locked():
+        with fitting_room:
+            print(threading.current_thread().name)
+            blueCtr += 1
+            time.sleep(1)
 
     # if all blue threads are finished or the number of blue threads
     # reached the quantum that was set, pass the access to green threads
-    if blueCtr == b or blueCtr % quantum == 0:
+    if (blueCtr == b or blueCtr % quantum == 0) and blue_access.locked():
         print("------Empty Fitting Room------")
         blue_access.release()
 
-    while green_access.locked() == False and greenCtr != g:
-        pass
+        while True:
+            if (green_access.locked() and blueCtr) < b or (
+                blueCtr == b and greenCtr == 0
+            ):
+                break
 
 
 n, b, g = list(map(int, input("Enter 3 space-separated integers (n, b, g): ").split()))
