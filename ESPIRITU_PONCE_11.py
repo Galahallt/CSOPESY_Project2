f3d4f46f4910be60a1991affcaa0f7cee825780e
Andre Ponce <75112557+su0h@users.noreply.github.com>
2022-06-11 01:21:50 +0800

    Reset

--- ESPIRITU_PONCE_11.py
+++ ESPIRITU_PONCE_11.py
@@ -16,72 +16,17 @@ import threading
 import time
 
 
-def green_enter():
-    global fitting_room, green_access, blue_access
-    global greenCtr, blueCtr, b, g, quantum
-
-    # wait for blue threads to release the lock for the fitting room
-    while True:
-        if not blue_access.locked():
-            break
-
-    # acquire the access to the fitting room for the green threads
-    if not green_access.locked():
-        green_access.acquire()
-        print("----------Green Only----------")
-
-    if green_access.locked():
-        with fitting_room:
-            print(threading.current_thread().name)
-            greenCtr += 1
-            time.sleep(1)
-
-    # if all green threads are finished or the number of green threads
-    # reached the quantum that was set, pass the access to blue threads
-    if (greenCtr == g or greenCtr % quantum == 0) and green_access.locked():
-        print("------Empty Fitting Room------")
-        green_access.release()
-
-    while True:
-        if (blue_access.locked() and blueCtr < b) or (blueCtr == b and greenCtr == g):
-            break
-
-
 def blue_enter():
-    global fitting_room, green_access, blue_access
-    global greenCtr, blueCtr, b, g, quantum
 
-    # wait for green threads to release the lock for the fitting room
-    while True:
-        if not green_access.locked():
-            break
-
-    # acquire the access to the fitting room for the blue threads
-    if not blue_access.locked():
-        blue_access.acquire()
-        print("----------Blue Only-----------")
-
-    if blue_access.locked():
-        with fitting_room:
-            print(threading.current_thread().name)
-            blueCtr += 1
-            time.sleep(1)
-
-    # if all blue threads are finished or the number of blue threads
-    # reached the quantum that was set, pass the access to green threads
-    if (blueCtr == b or blueCtr % quantum == 0) and blue_access.locked():
-        print("------Empty Fitting Room------")
-        blue_access.release()
-
-    while True:
-        if (green_access.locked() and blueCtr < b) or (blueCtr == b and greenCtr == g):
-            break
+def green_enter():
 
 
 n, b, g = list(map(int, input("Enter 3 space-separated integers (n, b, g): ").split()))
 
 # initialize n slots inside fitting room
 fitting_room = threading.BoundedSemaphore(value=n)
+
+print(fitting_room.__dict__)
 
 green_access = threading.Lock()
 blue_access = threading.Lock()
