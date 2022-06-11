15ee5abacbfa229ab2c854a545c5233b0ae510da
Andre Ponce <75112557+su0h@users.noreply.github.com>
2022-06-11 16:36:03 +0800

    Added documentation

--- ESPIRITU_PONCE_11.py
+++ ESPIRITU_PONCE_11.py
@@ -21,62 +21,100 @@ def blue_enter():
     global fitting_room, blue_access, green_access
     global blueCtr, greenCtr, b, g, quantum
 
+    # request for blue access into the room
     blue_access.acquire()
+    # request for access into the room
     fitting_room.acquire()
     
+    # +1 blue thread executed
     blue_ctr_access.acquire()
     blueCtr += 1
     blue_ctr_access.release()
 
+    # if first blue process and there are green threads waiting,
     if blueCtr % quantum == 1 and greenCtr <= g:
-        # print("Empty fitting room.")
+        # indicate that the waiting room is for blue threads only
         print("Blue Only.")
 
+        # if all green threads has been executed already,
         if greenCtr == g:
+            # then +1 for greenCtr to avoid printing "Blue Only."
+            # when blue threads are no longer giving the room to
+            # green threads
             green_ctr_access.acquire()
             greenCtr += 1
             green_ctr_access.release()
 
-    print(threading.current_thread().name)
+    # if blueCtr % quantum == 0 or blueCtr >= b:
+    #     print("Empty fitting room.")
 
+    # display thread details
+    print(threading.current_thread().name)
+    
+    # do something
     time.sleep(0.5)
+
+    # release the acquired fitting room lock / semaphore value
     fitting_room.release()
 
+    # if there are green threads waiting to enter the fitting room.
     if greenCtr < g:
+        # create a lock that can be acquired by a green thread
         green_access.release()
+    # else, just release the lock for this blue slot
     else:
         blue_access.release()
+
 
 
 def green_enter():
     global fitting_room, blue_access, green_access
     global blueCtr, greenCtr, b, g, quantum
 
+    # request for green access into the room
     green_access.acquire()
+    # request for access into the room
     fitting_room.acquire()
 
+    # +1 green thread executed
     green_ctr_access.acquire()
     greenCtr += 1
     green_ctr_access.release()
 
+    # if first green process and there are blue threads waiting,
     if greenCtr % quantum == 1 and blueCtr <= b:
-        # print("Empty fitting room.")
+        # indicate that the waiting room is for green threads only
         print("Green Only.")
 
+        # if all blue threads has been executed already,
         if blueCtr == b:
+            # then +1 for blueCtr to avoid printing "Green Only."
+            # when green threads are no longer giving the room to
+            # blue threads
             blue_ctr_access.acquire()
             blueCtr += 1
             blue_ctr_access.release()
+
+    # if greenCtr % quantum == 0 or greenCtr >= g:
+    #     print("Empty fitting room.")
     
+    # display thread details
     print(threading.current_thread().name)
     
+    # do something
     time.sleep(0.5)
+
+    # release the acquired fitting room lock / semaphore value
     fitting_room.release()
 
+    # if there are blue threads waiting to enter the fitting room.
     if blueCtr < b:
+        # create a lock that can be acquired by a blue thread
         blue_access.release()
+    # else, just release the lock for this green slot
     else:
         green_access.release()
+
 
 
 n, b, g = list(map(int, input("Enter 3 space-separated integers (n, b, g): ").split()))
@@ -84,17 +122,15 @@ n, b, g = list(map(int, input("Enter 3 space-separated integers (n, b, g): ").sp
 # initialize n slots inside fitting room
 fitting_room = threading.BoundedSemaphore(value=n)
 
-print(fitting_room.__dict__)
-
 # green_access = threading.Lock()
 # blue_access = threading.Lock()
 
 quantum = n
 
+# use semaphores instead of mutex locks
+# for now, blue goes first
 blue_access = threading.Semaphore(value=quantum)
 green_access = threading.Semaphore(value=0)
-
-print(blue_access.__dict__)
 
 greenCtr = blueCtr = 0
 
